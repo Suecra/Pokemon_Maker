68fa5fc3c5a32424171e97704bd8d1d8e797f450
Suecra <31817546+Suecra@users.noreply.github.com>
2019-03-29 21:25:28 +0100

    Importer

--- PokémonAPI/PokeAPI Import/Importer.gd
+++ PokémonAPI/PokeAPI Import/Importer.gd
@@ -4,8 +4,8 @@ export(String) var api_endpoint
 export(String) var folder_name
 var base_url
 var json
+var api_item_list
 var request_result
-var count
 
 func import(base_url, destination):
 	self.base_url = base_url
@@ -13,14 +13,14 @@ func import(base_url, destination):
 	var directory = destination + "/" + folder_name
 	if !dir.dir_exists(directory):
 		dir.make_dir(directory)
-	yield(get_count(), "completed")
-	yield(do_request(api_endpoint + "/?limit=" + str(count)), "completed")
-	if json.result != null:
-		print(json.result)
+	yield(do_request(api_endpoint + "/?limit=9999"), "completed")
+	api_item_list = json.result
+	if api_item_list != null:
+		var count = api_item_list["count"]
 		for i in count - 1:
-			#yield(do_request(api_endpoint + "/" + str(i + 1)), "completed")
-			var dict = json.result["results"][i + 1]
-			var name = dict["name"]
+			yield(do_request(api_item_list["results"][i]["url"], true), "completed")
+			var api_item = json.result
+			var name = api_item["name"]
 			var file = File.new()
 			var scene
 			var item
@@ -32,24 +32,22 @@ func import(base_url, destination):
 				scene = PackedScene.new()
 				item = _create_item()
 				item.name = name
-			_import_item(item, dict)
+			_import_item(item, api_item)
 			scene.pack(item)
 			ResourceSaver.save(path, scene)
-			get_parent().get_node("ProgressBar").value = i / count * 100
+			get_parent().get_node("ProgressBar").value = (i / count) * 100
 	pass
 
-func do_request(url):
-	var full_url = base_url + "/" + url
+func do_request(url, absolute = false):
+	var full_url = url
+	if !absolute:
+		full_url = base_url + "/" + full_url
 	var http_request = get_parent().get_node("HTTPRequest")
-	http_request.request(full_url, [], false)
+	http_request.request(full_url, ["User-Agent: Pirulo/1.0 (Godot)", "Accept: */*"], false)
 	yield(http_request, "request_completed")
 	if request_result != 0:
 		do_log("Error requesting " + full_url + "! Error-Code " + request_result)
 	pass
-
-func get_count():
-	yield(do_request(api_endpoint + "/?limit=1"), "completed")
-	count = json.result["count"]
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	json = JSON.parse(body.get_string_from_utf8())
