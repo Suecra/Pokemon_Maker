fb1e592e5acff7dbc6eedb10334a2cec37428adc
Suecra <31817546+Suecra@users.noreply.github.com>
2019-04-17 12:35:54 +0200

    Textboxes

--- PokémonAPI/Common/Textboxes/Styles/TextboxStyle.gd
+++ PokémonAPI/Common/Textboxes/Styles/TextboxStyle.gd
@@ -4,6 +4,7 @@ extends Container
 
 export(String) var debug_text setget set_debug_text
 export(bool) var custom_border = false
+export(NodePath) var text_node
 export(Texture) var top_left_corner
 export(Texture) var top_right_corner
 export(Texture) var bottom_left_corner
@@ -11,11 +12,14 @@ export(Texture) var bottom_right_corner
 export(Texture) var horizontal_border
 export(Texture) var vertical_border
 
-func set_debug_text():
+func set_debug_text(value):
 	pass
 
 func _resize():
 	pass
 
-func _redraw():
-	pass
+func _show():
+	visible = true
+
+func _hide():
+	visible = false
