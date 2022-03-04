00e08bd0c945b21342bcea57a8ec6ae1ab14ae84
Suecra <31817546+Suecra@users.noreply.github.com>
2022-03-04 17:41:00 +0100

    Battle System tests

--- Tests/Scripts/Battle System/Layer 1/EffectTests.gd
+++ Tests/Scripts/Battle System/Layer 1/EffectTests.gd
@@ -1,34 +1,35 @@
 extends WATTest
 
 const Effect = preload("res://Source/Scripts/Battle System/Layer 1/Effect.gd")
-var effect: Effect
-
-func test_b() -> void:
-	pass
+const TestEffectFactory = preload("res://Tests/Scripts/Battle System/Layer 1/TestEffectFactory.gd")
+const EffectManager = preload("res://Source/Scripts/Battle System/Layer 1/EffectManager.gd")
+const Battlefield = preload("res://Source/Scripts/Battle System/Layer 0/Battlefield.gd")
+var effect: Reference
+var effect_manager: EffectManager
 
 func test_v() -> void:
-	pass
+	effect._register()
+	effect.v("nudge", [])
+	asserts.is_equal(1, effect.i)
 
 func test_set_name() -> void:
-	pass
-
-func test_i() -> void:
-	pass
+	effect.set_name("Burn")
+	asserts.is_true(effect.names.has("Burn"))
 
 func test_is_type() -> void:
-	pass
+	effect.names.append("Flinch")
+	effect.names.append("Confusion")
+	asserts.is_true(effect.is_type("Flinch"))
+	asserts.is_true(effect.is_type("Confusion"))
 
 func test_reg() -> void:
-	pass
-
-func test_f() -> void:
-	pass
-
-func test_arr() -> void:
-	pass
-
-func test_register() -> void:
-	pass
+	effect.reg("nudge", 1, L1Consts.SenderType.BATTLEFIELD)
+	asserts.is_equal(effect, effect_manager.registered_effects["nudge"][0].effect)
+	asserts.is_equal(1, effect_manager.registered_effects["nudge"][0].priority)
+	asserts.is_equal(L1Consts.SenderType.BATTLEFIELD, effect_manager.registered_effects["nudge"][0].sender_type)
 
 func pre() -> void:
-	effect = Effect.new()
+	var factory = TestEffectFactory.new()
+	effect = factory.create_effect("", Battlefield.new())
+	effect_manager = EffectManager.new()
+	effect.effect_manager = effect_manager
